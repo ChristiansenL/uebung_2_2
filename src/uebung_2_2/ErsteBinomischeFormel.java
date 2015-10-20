f5817a1990909a7a1b0e04aea4ab5994c9817f8a
ChristiansenL <lasse.christiansen@nordakademie.de>
2015-10-20 11:05:46 +0200

    Erste binomische Formel Lasse Christiansen

---
+++ src/uebung_2_2/ErsteBinomischeFormel.java
@@ -0,0 +1,16 @@
+package uebung_2_2;
+
+public class ErsteBinomischeFormel {
+
+	public static void main(String[] args) {
+	
+		double a = 2;
+		double b = 4;
+		
+		double Binomischeformel = (a+b)*(a+b);
+		
+		System.out.println("Die Lösung für die erste binomische Formel mit den Parametern a = " + a + ", b = " + b + " ist " + Binomischeformel + ".");
+
+	}
+
+}
